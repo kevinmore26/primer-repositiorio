b07b880a2d3e449a785c8ee674743ae8c9582787
kevinmore26 <74431425+kevinmore26@users.noreply.github.com>
2021-08-16 21:54:50 -0500

    primer cambio

---
+++ 01-variables.py
@@ -0,0 +1,19 @@
+#sdadsasda
+nombre ="kevin"
+apellido='skadasdkjsak'
+direccion = "calle o'higgins 125 "
+
+texto = """ hola amigos :
+buenas noches, les saluda su profe
+atentamente, 
+eduardo """
+
+texto2='''hola amigos:
+ya casi viene el break'''
+
+# las comillas triples es para respetar el salto de línea, 2 puntos etc
+
+año=2021
+estatura = 1.70
+
+print(type(nombre))
