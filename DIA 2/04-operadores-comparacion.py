ad96799e8d4fa4eaafe5ef8902aef15c9a8f9894
kevinmore26 <74431425+kevinmore26@users.noreply.github.com>
2021-08-17 20:04:44 -0500

    cambios

---
+++ DIA 2/04-operadores-comparacion.py
@@ -0,0 +1,12 @@
+#  == es igual que 
+# NOTA => en python no existe la triple doble comparación como en JS 
+# en python los boolean empiezan con ma¿ayuscula
+# True
+# False
+print(5==5)
+# != es diferente que
+print (5==5)
+
+# <, >, es menor y mayor que
+
+print(5<10)
