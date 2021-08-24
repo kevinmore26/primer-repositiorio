bfee59eea8b62d78c6e9d4b811e62c3246438728
kevinmore26 <74431425+kevinmore26@users.noreply.github.com>
2021-08-23 19:28:14 -0500

    comit

--- DIA 2/05-operadores-logicos.py
+++
@@ -1,9 +0,0 @@
-# y => significa que ambas comparaciones tienen que ser verdaderas para que sea verdadero
-# enJS es &&
-# en python es 'and'
-print((10>5) and(100<500))
-
-# o => significa que basta con que alguna de las comparasiones para que todo sea verdadero
-# en JS es || (alt+124)
-# en python es 'or
-print((10<5) or(50>1))
--- DIA 2/06-operadores-identidad.py
+++
@@ -1,8 +0,0 @@
-#  is => es
-# is not => no es
-frutas =['carambola','guayaba']
-fruta='carambola'
-
-# el ' is e 'is not' se usa más que todo para validar si las variables a comparar están apuntando a la misma dirección de memoria o no
-
-print(fruta is frutas)
